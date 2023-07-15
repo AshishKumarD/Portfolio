ee9f7cc86f3d6da8f8be978de0d96cd185d2185a
yuki <ashishkumard1098@gmail.com>
2023-07-15 20:28:12 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Welcome to Ashish's React website</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Name: Ashish Kumar D
+          
+        </p>
+        <p>
+          Skills: NA
+          
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Have a great day
+      </p>
+    </>
+  )
+}
+
+export default App
